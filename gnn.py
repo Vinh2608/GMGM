4df062b33a2ea2b05c42d80610934d28c594be24
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2022-01-06 12:27:51 +0700

    Optimation 2: use protein atoms for prediction

--- gnn.py
+++ gnn.py
@@ -19,7 +19,7 @@ class gnn(torch.nn.Module):
         self.layers1 = [d_graph_layer for i in range(n_graph_layer+1)]
         self.gconv1 = nn.ModuleList([GAT_gate(self.layers1[i], self.layers1[i+1], gpu=args.ngpu) for i in range(len(self.layers1)-1)]) 
         
-        self.FC = nn.ModuleList([nn.Linear(self.layers1[-1], d_FC_layer) if i==0 else
+        self.FC = nn.ModuleList([nn.Linear(self.layers1[-1]*2, d_FC_layer) if i==0 else
                                  nn.Linear(d_FC_layer, 1) if i==n_FC_layer-1 else
                                  nn.Linear(d_FC_layer, d_FC_layer) for i in range(n_FC_layer)])
         
@@ -40,9 +40,10 @@ class gnn(torch.nn.Module):
             c_hs2 = self.gconv1[k](c_hs, c_adjs2)
             c_hs = c_hs2-c_hs1
             c_hs = F.dropout(c_hs, p=self.dropout_rate, training=self.training)
-        c_hs = c_hs*c_valid.unsqueeze(-1).repeat(1, 1, c_hs.size(-1))
-        c_hs = c_hs.sum(1)
-        return c_hs
+        c_hs_l = c_hs*c_valid[0].unsqueeze(-1).repeat(1, 1, c_hs.size(-1))
+        c_hs_p = c_hs*c_valid[1].unsqueeze(-1).repeat(1, 1, c_hs.size(-1))
+        output = torch.cat([c_hs_l.sum(1), c_hs_p.sum(1)], 1)
+        return output
 
     def fully_connected(self, c_hs):
         regularization = torch.empty(len(self.FC)*1-1, device=c_hs.device)
