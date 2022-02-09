13d1ea8ce8169756d1fb3bd5387edad0709225e8
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2022-02-09 15:57:18 +0700

    Opt 4: Multihop attention

--- layers.py
+++ layers.py
@@ -5,7 +5,7 @@ from utils import *
 import time
 
 class GAT_gate(torch.nn.Module):
-    def __init__(self, n_in_feature, n_out_feature, gpu = 0):
+    def __init__(self, n_in_feature, n_out_feature, nhop, gpu = 0):
         super(GAT_gate, self).__init__()
         self.W = nn.Linear(n_in_feature, n_out_feature)
         #self.A = nn.Parameter(torch.Tensor(n_out_feature, n_out_feature))
@@ -13,6 +13,7 @@ class GAT_gate(torch.nn.Module):
         self.gate = nn.Linear(n_out_feature*2, 1)
         self.leakyrelu = nn.LeakyReLU(0.2)
         self.zeros = torch.zeros(1)
+        self.nhop = nhop
         if gpu > 0:
             self.zeros = self.zeros.cuda()
 
@@ -28,10 +29,15 @@ class GAT_gate(torch.nn.Module):
         #attention = F.dropout(attention, self.dropout, training=self.training)
         #h_prime = torch.matmul(attention, h)
         attention = attention*adj
-        h_prime = F.relu(torch.einsum('aij,ajk->aik',(attention, h)))
-       
-        coeff = torch.sigmoid(self.gate(torch.cat([x,h_prime], -1))).repeat(1,1,x.size(-1))
-        retval = coeff*x+(1-coeff)*h_prime
+        # h_prime = F.relu(torch.einsum('aij,ajk->aik',(attention, h)))
+        # coeff = torch.sigmoid(self.gate(torch.cat([x,h_prime], -1))).repeat(1,1,x.size(-1))
+        # retval = coeff*x+(1-coeff)*h_prime
+
+        retval = h
+        for _ in range(self.nhop):
+            az = F.relu(torch.einsum('aij,ajk->aik',(attention, retval)))
+            coeff = torch.sigmoid(self.gate(torch.cat([h, az], -1))).repeat(1, 1, h.size(-1))
+            retval = coeff * h + (1 - coeff) * az
         
         if not get_attention:
             return retval
